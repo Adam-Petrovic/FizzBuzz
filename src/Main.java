64d01a9e07c8887fb80a48518f7658069542448d
Adam Petrovic <adam.petrovic@mail.utoronto.ca>
2024-09-09 18:00:34 -0400

    luh calm push

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         FizzBuzz buzzyFish = new FizzBuzz();
-        buzzyFish.doFizzBuzz(5);
-
+        buzzyFish.main(args);
     }
 }
